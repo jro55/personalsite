6b9dcaf0f087e3b55877f49528ed8220492f656c
Josh Roman <joshproman@yahoo.com>
2015-11-10 15:48:06 -0700

    second commit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,10 +8,10 @@ angular.module('StarterApp')
 				templateUrl : '/html/home.html',
 				controller : 'AppCtrl'
 			})
-//			.when('/signup', {
-//				templateUrl : '/html/register.html',
-//				controller : 'mainController'
-//			})
+			.when('/work', {
+				templateUrl : '/html/work.html',
+				controller : 'AppCtrl'
+			})
 //            .when('/upcomingshows', {
 //                templateUrl : '/html/upcomingShows.html',
 //                controller : 'mainController'
@@ -28,22 +28,22 @@ angular.module('StarterApp')
 	}])
 
 
-angular.module('StarterApp')
-    .config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    .primaryPalette('red', {
-      'default': '400', // by default use shade 400 from the pink palette for primary intentions
-      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
-      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
-      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
-    })
-    // If you specify less than all of the keys, it will inherit from the
-    // default shades
-    .accentPalette('purple', {
-      'default': '200' // use shade 200 for default, and keep all other shades the same
-    });
-//    $mdThemingProvider.theme('default').dark()
-});
+//angular.module('StarterApp')
+//    .config(function($mdThemingProvider) {
+//  $mdThemingProvider.theme('default')
+//    .primaryPalette('red', {
+//      'default': '400', // by default use shade 400 from the pink palette for primary intentions
+//      'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+//      'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+//      'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+//    })
+//    // If you specify less than all of the keys, it will inherit from the
+//    // default shades
+//    .accentPalette('purple', {
+//      'default': '200' // use shade 200 for default, and keep all other shades the same
+//    });
+////    $mdThemingProvider.theme('default').dark()
+//});
 
 angular.module('StarterApp')
     .controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
@@ -98,6 +98,12 @@ angular.module('StarterApp')
             icon: "pageview"
         }
     ]
+    
+    $scope.showContacts = false;
+    
+    $scope.clickContacts = function() {
+        $scope.showContacts = !$scope.showContacts;
+    }
  
 }]);
 
