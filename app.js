bed4416a9decaea1317897c34bb6ed229a2f7e7e
Josh Roman <joshproman@yahoo.com>
2016-03-16 14:32:26 -0600

    fixed wording

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get('/', function(req, res){
 });
 
 // Creating Server and Listening for Connections \\
-var port = 80;
+var port = 3000;
 app.listen(port, function(){
   console.log('Server running on port ' + port);
 
